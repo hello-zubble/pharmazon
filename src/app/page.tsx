ff88cf33fbe49f0317eb46acb336cc197e76d4be
hello-zubble <hello@zubble.co>
2024-01-08 17:16:26 +0530

    Refactor code and add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,40 @@
 import Image from 'next/image'
-import {specialties,doctors,nearByClinics} from "@/lib/data/index";
+import { specialties, doctors, nearByClinics } from "@/lib/data/index";
 import DoctorProfile from '@/components/DoctorProfile';
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (<>
+    <Hero />
+    <section className=" bg-blue-100 py-10">
+      <div className='container mx-auto'>
 
-  <section className=" bg-blue-100 py-10">
-    <div className='container mx-auto'>
-
-<div className="flex items-center justify-center gap-1">
-   
-
-   
+        <div className="flex items-center justify-center gap-1">
 
 
-    {doctors.map((doctor)=>{
+
+
+
+          {doctors.map((doctor) => {
             return (
               <DoctorProfile
                 key={doctor.doctorName}
-                {...doctor}/>
+                {...doctor} />
             )
           })}
 
           <div className="text-container">
-         
-<h2 className='text-3xl font-bold'> Book Your Next Medical
-Visit Online Today!</h2>
 
-<p>Explore their expertise, credentials, and specialties to find the right doctor for your healthcare needs.</p>
+            <h2 className='text-3xl font-bold'> Book Your Next Medical
+              Visit Online Today!</h2>
+
+            <p>Explore their expertise, credentials, and specialties to find the right doctor for your healthcare needs.</p>
           </div>
 
-    
 
-    </div>
-    </div>
-  </section>
+
+        </div>
+      </div>
+    </section>
   </>)
 }
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, MouseEventHandler } from 'react';
 
 type PrimaryButtonProps = {
     children: ReactNode;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+    type?: 'button' | 'submit' | 'reset';
 };
+    
 type LinkButtonProps = {
     children: ReactNode;
     href: string;
@@ -17,6 +20,14 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({ children }) => {
     );
 };
 
+const SecondaryButton: React.FC<PrimaryButtonProps> = ({ children }) => {
+    return (
+        <button className="bg-ui-secondary hover:bg-ui-secondary/90 text-white py-3 px-6 rounded">
+            {children}
+        </button>
+    );
+}
+
 const LinkButton: React.FC<LinkButtonProps> = ({ children, href }) => {
     return (
         <Link href={href} className="text-ui-primary hover:text-ui-primary/90py-3 px-6">
@@ -25,4 +36,4 @@ const LinkButton: React.FC<LinkButtonProps> = ({ children, href }) => {
     );
 };
 
-export { PrimaryButton,LinkButton };
+export { PrimaryButton,SecondaryButton,LinkButton };
