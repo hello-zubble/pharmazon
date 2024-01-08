96e25519dc196aecf5eb6a6b33ba46a8c369f4d4
rajatmathew <rajatxmathew@gmail.com>
2024-01-08 16:49:44 +0530

    upd font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { DM_Sans } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const DM = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Pharmazon',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={DM.className}>
         <Navbar />
         <main>
           {children}
